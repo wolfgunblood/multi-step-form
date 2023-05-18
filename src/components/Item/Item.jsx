abdf01d9706eed9bc5e8f290fb2b51f4172c3789
wolfgunblood <jalajdorai1@gmail.com>
2023-05-18 17:58:30 +0530

    SideBar Updated

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./Item.scss";
 
 const Item = ({item,i}) => {
   return (
-    <div>
-        <div>{i}</div>
-        <div>
-            <h5>{`STEP ${i}`}</h5>
-            <h5>{item}</h5>
+    <div className='item-container'>
+        <div className='item-number'>{i}</div>
+        <div className='item-content'>
+            <div>{`STEP ${i}`}</div>
+            <div>{item}</div>
         </div>
     </div>
   )
